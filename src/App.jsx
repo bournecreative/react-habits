bc880ea540afbb8fcce34f98be2fcb1308dd6945
Christian <bournecreative@gmail.com>
2023-12-12 16:34:53 -0600

    refactored get, edit, create and delete books functions to make requests to json server.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { BookCreate } from "./components/bookCreate/BookCreate";
 import { BookList } from "./components/bookList/BookList";
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const handleEditTitle = (id, title) => {
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const response = await axios.get("http://localhost:3001/books");
+      setBooks([...response.data]);
+    };
+    if (isMounted.current) {
+      getBooks();
+    }
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  const handleEditTitle = async (id, title) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: title,
+    });
+
     const updatedBook = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: title };
+        return { ...book, ...response.data };
       }
       return book;
     });
     setBooks([...updatedBook]);
   };
 
-  const handleDeleteBook = (target) => {
+  const handleDeleteBook = async (target) => {
+    const response = await axios.put(`http://localhost:3001/books/${target}`);
     const updatedBooks = books.filter((b) => {
       return b.id !== target;
     });
     setBooks(updatedBooks);
   };
 
-  const handleCreateBook = (newBook) => {
-    const updatedBooks = [
-      ...books,
-      { id: Math.round(Math.random() * 9999), title: newBook },
-    ];
-    setBooks(updatedBooks);
+  const handleCreateBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+    console.log(response.data);
+    setBooks([...books, response.data]);
   };
 
   return (
